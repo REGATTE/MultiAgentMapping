ce6f82cf753d1b67760a9c774ffbd37523149fe8
Ashok Kumar J <ashok.kumarj@icloud.com>
2025-01-07 21:05:04 -0800

    updated config setup.py to recursively read through config subforlders

--- project_config/setup.py
+++ project_config/setup.py
@@ -4,17 +4,25 @@ from glob import glob
 
 package_name = 'project_config'
 
+# Collect all YAML and config files, including subfolders
+data_files = [
+    ('share/ament_index/resource_index/packages',
+        ['resource/' + package_name]),
+    ('share/' + package_name, ['package.xml']),
+]
+
+# Recursively include files from the config directory
+for root, dirs, files in os.walk('config'):
+    file_paths = [os.path.join(root, f) for f in files]  # Collect all files in the directory
+    if file_paths:  # Add only if files exist
+        dest = os.path.join('share', package_name, root)  # Destination path
+        data_files.append((dest, file_paths))
+
 setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        # Install config folder
-        ('share/' + package_name + '/config', glob('config/*.yaml'))  # All YAML files
-    ],
+    data_files=data_files,
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='regastation',
