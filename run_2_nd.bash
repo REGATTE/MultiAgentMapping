c782b2891b2e19160813b19ac7fa6249ca14caa3
Ashok Kumar J <ashok.kumarj@icloud.com>
2025-03-10 23:33:20 -0700

    testing no domain test

---
+++ run_2_nd.bash
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+## ==============================================================================
+## Robot scout_1_1
+# Launch the Isaac Sim point cloud publisher in a new terminal
+echo "Launching Isaac Sim Point Cloud Full Publisher for Scout_1_1 in a new terminal..."
+gnome-terminal -- bash -c "
+    ros2 launch isaac_sim_pointcloud_full_publisher full_pcd_pub.launch.py robot_namespace:=a config_file:=velodyne_vls_128.yaml; 
+    exec bash
+"
+
+# Launch the distributed mapping node in another new terminal
+echo "Launching distributed mapping with the specified parameters for Scout_1_1 in a new terminal..."
+gnome-terminal -- bash -c "
+    ros2 launch multi_agent_mapping run_params.launch.py params:=params_scout_1_1.yaml  namespace:=/a;  
+    exec bash
+"
+
+gnome-terminal -- bash -c "
+    ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args --remap cmd_vel:=/a/cmd_vel;
+    exec bash
+"
+
+## ==============================================================================
+## Robot scout_2_2
+# Launch the Isaac Sim point cloud publisher in a new terminal
+echo "Launching Isaac Sim Point Cloud Full Publisher for Scout_2_2 in a new terminal..."
+gnome-terminal -- bash -c "
+    ros2 launch isaac_sim_pointcloud_full_publisher full_pcd_pub.launch.py robot_namespace:=b config_file:=velodyne_vls_128.yaml; 
+    exec bash
+"
+
+# Launch the distributed mapping node in another new terminal
+echo "Launching distributed mapping with the specified parameters for Scout_2_2 in a new terminal..."
+gnome-terminal -- bash -c "
+    ros2 launch multi_agent_mapping run_params.launch.py params:=params_scout_2_2.yaml  namespace:=/b;  
+    exec bash
+"
+
+gnome-terminal -- bash -c "
+    ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args --remap cmd_vel:=/b/cmd_vel;
+    exec bash
+"
+
+## ==============================================================================
+## Distributed
+echo "Launching RVIZ"
+gnome-terminal -- bash -c "
+    ros2 run rviz2 rviz2 -d /home/regastation/workspaces/masters_ws/src/MultiAgentMapping/ros_packages/multiAgentMapping/config/rviz/dist_mapping.rviz;
+    exec bash
+"
+
+echo "LoopVisualizationNode"
+gnome-terminal -- bash -c "
+    ros2 run multi_agent_mapping multi_agent_mapping_loopVisualizationNode;
+    exec bash
+"
+
